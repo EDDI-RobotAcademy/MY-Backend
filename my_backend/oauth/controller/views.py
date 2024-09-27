c0702fe9f60f65c682489445639a97ee0764ddb1
JUNGUIHEON <jungui4366@gmail.com>
2024-09-27 15:45:56 +0900

    feat: [정의헌] kakao 로그인 기능을 구현합니다. [MYBD-10]

---
+++ my_backend/oauth/controller/views.py
@@ -0,0 +1,67 @@
+import uuid
+
+from django.http import JsonResponse
+from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from account.service.account_service_impl import AccountServiceImpl
+from oauth.serializer.kakao_oauth_access_token_serializer import KakaoOauthAccessTokenSerializer
+from oauth.serializer.kakao_oauth_url_serializer import KakaoOauthUrlSerializer
+from oauth.service.oauth_service_impl import OauthServiceImpl
+from oauth.service.redis_service_impl import RedisServiceImpl
+
+class OauthView(viewsets.ViewSet):
+    oauthService = OauthServiceImpl.getInstance()
+    redisService = RedisServiceImpl.getInstance()
+    accountService = AccountServiceImpl.getInstance()
+
+    def kakaoOauthURI(self, request):
+        url = self.oauthService.kakaoLoginAddress()
+        print(f"url:", url)
+        serializer = KakaoOauthUrlSerializer(data={ 'url': url })
+        serializer.is_valid(raise_exception=True)
+        print(f"validated_data: {serializer.validated_data}")
+        return Response(serializer.validated_data)
+
+    def kakaoAccessTokenURI(self, request):
+        serializer = KakaoOauthAccessTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        code = serializer.validated_data['code']
+
+        try:
+            accessToken = self.oauthService.requestAccessToken(code)
+            print(f"accessToken: {accessToken}")
+            return JsonResponse({'accessToken': accessToken})
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+
+    def kakaoUserInfoURI(self, request):
+        accessToken = request.data.get('access_token')
+        print(f'accessToken: {accessToken}')
+
+        try:
+            user_info = self.oauthService.requestUserInfo(accessToken)
+            return JsonResponse({'user_info': user_info})
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+
+    def redisAccessToken(self, request):
+        try:
+            email = request.data.get('email')
+            access_token = request.data.get('accessToken')
+            print(f"redisAccessToken -> email: {email}")
+
+            account = self.accountService.findAccountByEmail(email)
+            if not account:
+                return Response({'error': 'Account not found'}, status=status.HTTP_404_NOT_FOUND)
+
+            userToken = str(uuid.uuid4())
+            self.redisService.store_access_token(account.id, userToken)
+            accountId = self.redisService.getValueByKey(userToken)
+            print(f"accountId: {accountId}")
+
+            return Response({ 'userToken': userToken }, status=status.HTTP_200_OK)
+        except Exception as e:
+            print('Error storing access token in Redis:', e)
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
---
+++ my_backend/oauth/serializer/kakao_oauth_url_serializer.py
@@ -0,0 +1,4 @@
+from rest_framework import serializers
+
+class KakaoOauthUrlSerializer(serializers.Serializer):
+    url = serializers.URLField()
