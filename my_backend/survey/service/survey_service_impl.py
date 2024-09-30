649173ccf29a0c89e804e37bca39def33bca4155
JUNGUIHEON <jungui4366@gmail.com>
2024-09-30 16:21:46 +0900

    fix: [정의헌] DB 엔티티 수정 [MYBD-12]

--- my_backend/survey/service/survey_service_impl.py
+++ my_backend/survey/service/survey_service_impl.py
@@ -2,7 +2,7 @@ from account.repository.account_repository_impl import AccountRepositoryImpl
 from survey.repository.survey_answer_repository_impl import SurveyAnswerRepositoryImpl
 from survey.repository.survey_question_repository_impl import SurveyQuestionRepositoryImpl
 from survey.repository.survey_repository_impl import SurveyRepositoryImpl
-from survey.repository.custom_selection_repository_impl import SurveySelectionRepositoryImpl
+from survey.repository.custom_selection_repository_impl import CustomSelectionRepositoryImpl
 from survey.service.survey_service import SurveyService
 
 
@@ -15,7 +15,7 @@ class SurveyServiceImpl(SurveyService):
             cls.__instance.__surveyRepository = SurveyRepositoryImpl.getInstance()
         cls.__instance.__accountRepository = AccountRepositoryImpl.getInstance()
         cls.__instance.__surveyQuestionRepository = SurveyQuestionRepositoryImpl.getInstance()
-        cls.__instance.__surveySelectionRepository = SurveySelectionRepositoryImpl.getInstance()
+        cls.__instance.__surveySelectionRepository = CustomSelectionRepositoryImpl.getInstance()
         cls.__instance.__surveyAnswerRepository = SurveyAnswerRepositoryImpl.getInstance()
 
         return cls.__instance
