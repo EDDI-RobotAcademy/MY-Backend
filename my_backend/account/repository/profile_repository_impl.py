f83f4775b569a630232715b8ab9239f69d95de6e
JeongAram <arongj6317@gmail.com>
2024-09-30 15:17:18 +0900

    fix: [정아람] 코드 삭제 오류로 재업로드 [MYBD-8, 9]

--- my_backend/account/repository/profile_repository_impl.py
+++ my_backend/account/repository/profile_repository_impl.py
@@ -18,6 +18,28 @@ class ProfileRepositoryImpl(ProfileRepository):
 
         return cls.__instance
 
+    def findByEmail(self, email):
+        try:
+            profile = Profile.objects.get(email=email)
+            return profile
+        except Profile.DoesNotExist:
+            print(f"email로 profile을 찾을 수 없습니다.: {email}")
+            return None
+        except Exception as e:
+            print(f"error occurred during email duplicate check: {e}")
+            return None
+
+    def findByNickname(self, nickname):
+        try:
+            profile = Profile.objects.get(nickname=nickname)
+            return profile
+        except Profile.DoesNotExist:
+            print(f"nickname으로 profile을 찾을 수 없습니다.: {nickname}")
+            return None
+        except Exception as e:
+            print(f"error occurred during nickname duplicate check: {e}")
+            return None
+
     def create(self, nickname, email, account):
         profile = Profile.objects.create(nickname=nickname, email=email, account=account)
         return profile
