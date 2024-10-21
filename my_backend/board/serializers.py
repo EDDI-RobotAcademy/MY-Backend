7bd847cb32bd0dd8e291b3757216deca29a8aa22
JUNGUIHEON <jungui4366@gmail.com>
2024-10-21 10:17:40 +0900

    fix: [정의헌] board table에 account값을 받아오도록 변경 [MYBD-60]

--- my_backend/board/serializers.py
+++ my_backend/board/serializers.py
@@ -1,41 +1,30 @@
 from rest_framework import serializers
 
-from board.entity.BoardCategory import BoardCategory
+from board.entity.boardcategory import BoardCategory
 from board.entity.models import Board
 
 
+
 class BoardSerializer(serializers.ModelSerializer):
-    category_id = serializers.PrimaryKeyRelatedField(
-        queryset=BoardCategory.objects.all(),
-        source='category',
-        write_only=True
-    )
+    category_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = Board
-        fields = ['boardId', 'title', 'writer', 'content', 'regDate', 'updDate', 'category_id', 'categoryBoardId']
+        fields = ['boardId', 'title', 'account', 'content', 'regDate', 'updDate', 'category_id', 'categoryBoardId']
         read_only_fields = ['boardId', 'regDate', 'updDate', 'categoryBoardId']
 
-    class BoardSerializer(serializers.ModelSerializer):
-        category_id = serializers.IntegerField(write_only=True)
+    def validate_category_id(self, value):
+        try:
+            BoardCategory.objects.get(pk=value)
+        except BoardCategory.DoesNotExist:
+            raise serializers.ValidationError(f"Category with id {value} does not exist.")
+        return value
 
-        class Meta:
-            model = Board
-            fields = ['boardId', 'title', 'writer', 'content', 'regDate', 'updDate', 'category_id', 'categoryBoardId']
-            read_only_fields = ['boardId', 'regDate', 'updDate', 'categoryBoardId']
-
-        def validate_category_id(self, value):
-            try:
-                BoardCategory.objects.get(pk=value)
-            except BoardCategory.DoesNotExist:
-                raise serializers.ValidationError(f"Category with id {value} does not exist.")
-            return value
-
-        def create(self, validated_data):
-            category_id = validated_data.pop('category_id')
-            category = BoardCategory.objects.get(pk=category_id)
-            board = Board.objects.create(category=category, **validated_data)
-            return board
+    def create(self, validated_data):
+        category_id = validated_data.pop('category_id')
+        category = BoardCategory.objects.get(pk=category_id)
+        board = Board.objects.create(category=category, **validated_data)
+        return board
 
 
 class BoardCategorySerializer(serializers.ModelSerializer):
