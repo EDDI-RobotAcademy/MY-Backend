5b95b09dfec98958166faf98c6102c3e29a56674
JUNGUIHEON <jungui4366@gmail.com>
2024-10-24 17:13:19 +0900

    feat:[정의헌] 성향분석 요청을 request와 answer 테이블로 분리합니다. [MYBD-97]

--- my_backend/user_analysis/entity/user_analysis_answer.py
+++ my_backend/user_analysis/entity/user_analysis_answer.py
@@ -4,33 +4,18 @@ from user_analysis.entity.user_analysis_custom_selection import UserAnalysisCust
 from user_analysis.entity.user_analysis_fixed_boolean_selection import UserAnalysisFixedBooleanSelection
 from user_analysis.entity.user_analysis_fixed_five_score_selection import UserAnalysisFixedFiveScoreSelection
 
-from user_analysis.entity.user_analysis import UserAnalysis
 from user_analysis.entity.user_analysis_question import UserAnalysisQuestion
+from user_analysis.entity.user_analysis_request import UserAnalysisRequest
 
 
 class UserAnalysisAnswer(models.Model):
-    user_analysis = models.ForeignKey(UserAnalysis, related_name='answers', on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    request = models.ForeignKey(UserAnalysisRequest, related_name='answers', on_delete=models.CASCADE())
     question = models.ForeignKey(UserAnalysisQuestion, related_name='answers', on_delete=models.CASCADE)
     answer_text = models.TextField(blank=True, null=True)
     five_score_selection = models.ForeignKey(UserAnalysisFixedFiveScoreSelection, blank=True, null=True, on_delete=models.SET_NULL)
     boolean_selection = models.ForeignKey(UserAnalysisFixedBooleanSelection, blank=True, null=True, on_delete=models.SET_NULL)
     custom_selection = models.ForeignKey(UserAnalysisCustomSelection, blank=True, null=True, on_delete=models.SET_NULL)
-    account = models.ForeignKey(Account, related_name='user_analysis_answers', on_delete=models.CASCADE, null=True, default=None)
-
-    response_order = models.IntegerField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def save(self, *args, **kwargs):
-        if self.account and self.user_analysis and self.question and self.response_order is None:
-            last_order = UserAnalysisAnswer.objects.filter(
-                account=self.account,
-                user_analysis=self.user_analysis,
-                question=self.question
-            ).aggregate(models.Max('response_order'))['response_order__max']
-
-            self.response_order = (last_order or 0) + 1
-
-        super(UserAnalysisAnswer, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"Answer to {self.question.question_text}"
---
+++ my_backend/user_analysis/repository/user_analysis_request_repository_impl.py
@@ -0,0 +1,29 @@
+from account.entity.account import Account
+from user_analysis.entity.user_analysis import UserAnalysis
+from user_analysis.entity.user_analysis_request import UserAnalysisRequest
+from user_analysis.repository.user_analysis_request_repository import UserAnalysisRequestRepository
+
+
+class UserAnalysisRequestRepositoryImpl(UserAnalysisRequestRepository):
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+
+        return cls.__instance
+
+    @classmethod
+    def getInstance(cls):
+        if cls.__instance is None:
+            cls.__instance = cls()
+
+        return cls.__instance
+
+    def create(self, account_id, user_analysis_id):
+        account = Account.objects.get(id = account_id)
+        user_analysis = UserAnalysis.objects.get(id = user_analysis_id)
+        user_analysis_request = UserAnalysisRequest(account=account, user_analysis=user_analysis)
+        user_analysis_request.save()
+
+        return user_analysis_request
