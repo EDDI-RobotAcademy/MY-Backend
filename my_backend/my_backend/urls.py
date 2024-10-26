1bd996cb46d12c71a6449a7e58e060a013a9cc1b
JeongAram <arongj6317@gmail.com>
2024-10-26 16:11:13 +0900

    fix: [정아람] 카카오페이 url 삭제 [MYBD]

--- my_backend/my_backend/urls.py
+++ my_backend/my_backend/urls.py
@@ -35,7 +35,6 @@ urlpatterns = [
     path('custom_strategy_history/', include('custom_strategy_history.urls')),
     path('viewCount/', include('viewCount.urls')),
     path('free_community_comment/', include('free_community_comment.urls')),
-    path('kakao_pay/', include('kakao_pay.urls')),
     path('ai-request/', include('ai_request.urls')),
     path('tosspayments/', include('tosspayments.urls')),
     path('smart_content/', include('smart_content.urls')),
